bdc100de7fb9a216ea33c570ca79ef5c9ec14107
Luke Patterson <lsp52@cornell.edu>
2019-07-02 11:22:51 -0400

    u

--- 3i_manual_list_changes.py
+++ 3i_manual_list_changes.py
@@ -5,30 +5,17 @@ df=pd.read_csv('step_3_work/output/full_retailer_list_orig.csv')
 
 # 7 retailers have shifted states; going to overwrite these values
 # columns
-cols = ['DBA Name_update', 'Full Address_update', 'Latitude_update',
-    'Longitude_update', 'Address Line 1_update', 'City_update', 'State_update',
-    'Zip_update', 'County_update']
-
-impaq_ids = [0,13,54,379,569,751,1586]
+impaq_ids = [13,54,379,569,751,1586]
 update_rows = df['IMPAQ_ID'].apply(lambda x: x in impaq_ids)
-update_cols = [i for i in cols if '_update' in i]
+change_cols = [i for i in df.columns if '_changes' in i]
+update_cols = [i for i in df.columns if '_update' in i]
 other_cols = ['Coordinates', 'On Tribe Land', 'Miles to Nearest Tribe',
     'Assoc Tribe', 'Assoc Res', 'Nearest Tribe', 'Nearest Res','biz_status']
 
-df.loc[update_rows , update_cols] = 'No changes'
+df.loc[update_rows , change_cols] = "Can't validate"
+for i in update_cols:
+    orig_var = i.replace('_update','')
+    df.loc[update_rows, i] = df.loc[update_rows, orig_var]
 df.loc[update_rows , other_cols] = "Can't validate"
 
-
-
-
-
-
-# NEXT PART OF THE CODE SHOULD STAY AT END OF FILE
-# revalidate _changes columns
-column_names=['DBA Name','Address Line 1','City','State','Zip',
-	'Latitude','Longitude','Full Address','County']
-for i in column_names:
-    df[i+'_changes'] = df[i+'_update']
-    df.loc[df[i+'_changes'] == df[i+'_update'], i+'_changes'] = 'Correct'
-    df.loc[df['biz_status'] == "Can't Validate", i+'_changes'] = "Can't Validate"
 df.to_csv('step_3_work/output/full_retailer_list.csv')
